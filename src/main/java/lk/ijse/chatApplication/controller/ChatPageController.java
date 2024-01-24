cdd18b39133fa1a126a279e42b32e239eb68a10c
MalithShehan <shehansandakalum2003@gmail.com>
2024-01-24 21:10:31 +0530

    Done

---
+++ src/main/java/lk/ijse/chatApplication/controller/ChatPageController.java
@@ -0,0 +1,40 @@
+package lk.ijse.chatApplication.controller;
+
+import com.jfoenix.controls.JFXTextArea;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ChatPageController {
+
+    @FXML
+    private AnchorPane chatPage;
+
+    @FXML
+    private JFXTextArea txtAreaChat;
+
+    @FXML
+    private TextField txtTypeMsg;
+
+    @FXML
+    void imageBTnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void imojiBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void plusBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sendMsgOnAction(ActionEvent event) {
+
+    }
+
+}
--- src/main/java/lk/ijse/chatApplication/controller/LoginPageController.java
+++ src/main/java/lk/ijse/chatApplication/controller/LoginPageController.java
@@ -1,16 +1,12 @@
 package lk.ijse.chatApplication.controller;
 
+import com.mysql.cj.log.Log;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import lk.ijse.chatApplication.bo.BOFactory;
-import lk.ijse.chatApplication.bo.custom.RegisterBO;
-import lk.ijse.chatApplication.dao.DAOFactory;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -22,48 +18,19 @@ public class LoginPageController {
     private AnchorPane Login;
 
     @FXML
-    private TextField txtEmail;
-
-    @FXML
-    private PasswordField txtPassword;
-
-    RegisterBO registerBO = (RegisterBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.REGISTER);
-
-    @FXML
-    void forgetPasswordBtnOnAction(MouseEvent event) {
-
-    }
+    private TextField txtName;
 
     @FXML
     void loginBtnOnAction(ActionEvent event) {
-        String email = txtEmail.getText();
-        String password = txtPassword.getText();
-        try {
-            boolean isIn = registerBO.searchUser(email, password);
-            if (!isIn) {
-                new Alert(Alert.AlertType.WARNING,"Invalid Email or Password").show();
-            }
-            else {
-                Login.getChildren().clear();
-                Login.getChildren().add(FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/view/chat_page.fxml"))));
-            }
-            } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @FXML
-    void signUpBtnOnAction(MouseEvent event) {
         try {
             Login.getChildren().clear();
-            Login.getChildren().add(FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/view/register_page.fxml"))));
+            Login.getChildren().add(FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/view/chat_page.fxml"))));
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
+
 
 }
