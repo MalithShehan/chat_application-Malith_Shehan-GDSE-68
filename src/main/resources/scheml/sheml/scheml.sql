37bf88049cf9e9b694a7a64fd46687cc2fd3f919
MalithShehan <shehansandakalum2003@gmail.com>
2024-01-25 09:59:39 +0530

    Recorection

--- src/main/resources/scheml/sheml/scheml.sql
+++
@@ -1,9 +0,0 @@
-create database chatApplication;
-
-use chatApplication;
-
-create table User(
-                     name varchar(15),
-                     email varchar(35) primary key ,
-                     password varchar(155)
-);
